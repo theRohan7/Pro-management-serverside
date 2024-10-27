495783538ca4bc537c65eef357dceea9796aa3a2
Rohan sahu <therohansahu7@gmail.com>
2024-10-27 10:09:07 +0530

    chnaged port to 9000

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ connectDB()
    })
 })
 .then( () => {
-   app.listen(process.env.PORT || 8000, () => {
+   app.listen(process.env.PORT || 9000, () => {
       console.log(` Server is running on PORT: ${process.env.PORT}`)
    })
 
